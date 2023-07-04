23cf4fc02c0e993217690e7ae1784c312547859b
regev654 <regev654@gmail.com>
2023-07-04 18:37:30 +0300

    adding conditions

---
+++ HW5/part2/CellType.h
@@ -0,0 +1,8 @@
+#ifndef PART2_CELLTYPE_H
+#define PART2_CELLTYPE_H
+
+enum CellType{
+    EMPTY, X, A, B, C, D, E, F, G, H, I, J, K, O, P, Q, R
+};
+
+#endif //PART2_CELLTYPE_H
--- HW5/part2/main.cpp
+++ HW5/part2/main.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 
 #include "List.h"
+#include "Conditional.h"
 
 template<int T>
 struct Int
@@ -60,8 +61,15 @@ int main()
 
 
 
+    typedef typename Conditional<(0 != 1), Int<0>, Int<1>>::value test1; // = Int<0>
+    typedef typename Conditional<(0 == 1), Int<0>, Int<1>>::value test2; // = Int<1>
+    assert(typeid(test1) == typeid(Int<0>));
+    assert(typeid(test2) == typeid(Int<1>));
 
-
+    int val = ConditionalInteger<(0 != 1), 0, 1>::value; // = 0
+    assert(val == 0);
+    val = ConditionalInteger<(0 == 1), 0, 1>::value; // = 1
+    assert(val == 1);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
